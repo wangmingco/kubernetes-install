f4c9d6c3c0e6c7868d0d0b98b050500e082e2eb7
wangming <wangming.co@hotmail.com>
2021-07-16 08:47:45 +0000

    update install_harbor.sh.

--- install_harbor.sh
+++ install_harbor.sh
@@ -74,7 +74,9 @@ if [[ -n "${file_exist}" ]]; then
   echo "安装harbor完成"
   echo "请在浏览器访问: http://${publicIp}:10080/harbor"
   echo "请在命令后访问: docker login -u admin -p Harbor12345 http://${publicIp}:10080"
-
+  
+  docker login -u admin -p Harbor12345 http://${publicIp}:10080
+  
   echo "执行仓库测试命令"
   echo "拉取最新centos: docker pull vish/stress"
   docker pull vish/stress
