0c88ba2992a048df6bd4d850558f9118964e8455
wangming <wangming.co@hotmail.com>
2021-07-16 15:12:34 +0800

    format install_kube_cloud_server.sh to linux file format

--- install_kube_cloud_server.sh
+++ install_kube_cloud_server.sh
@@ -1,24 +1,24 @@
+#!/bin/bash
 
-function clone-kube-cloud-server() {
-    git clone https://gitee.com/wangmingco/kube-cloud-server.git ~/kube-cloud-server
+function clone_kube_cloud_server() {
+        git clone https://gitee.com/wangmingco/kube-cloud-server.git ~/kube-cloud-server
 }
 
-function package-server() {
-    cd ~/kube-cloud-server
-    mvn clean package
+function package_server() {
+        cd ~/kube-cloud-server
+        mvn clean package
 }
 
-function init-database() {
-    mysql -uroot -proot < ~/kube-cloud-server/doc/sql/db.sql
+function init_database() {
+        mysql -uroot -proot < ~/kube-cloud-server/doc/sql/db.sql
 }
 
-function start-server() {
-    nohup java -jar ./target/kube-cloud-server-0.1.jar >kube-cloud-server-nohup.log 2>&1 &
-
-    jps -l | grep "kube-cloud-server" | awk '{print $1}' > server.pid
+function start_server() {
+        nohup java -jar ./target/kube-cloud-server-0.1.jar >kube-cloud-server-nohup.log 2>&1 &
+        jps -l | grep "kube-cloud-server" | awk '{print $1}' > server.pid
 }
 
-clone-kube-cloud-server
-package-server
-init-database
-start-server
+clone_kube_cloud_server
+package_server
+init_database
+start_server
