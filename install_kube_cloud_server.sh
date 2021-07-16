13ab20f7f4f742195862ac815b0979f9e0267d12
wangming <wangming.co@hotmail.com>
2021-07-16 09:28:16 +0000

    update install_kube_cloud_server.sh.

--- install_kube_cloud_server.sh
+++ install_kube_cloud_server.sh
@@ -16,6 +16,8 @@ function init_database() {
 function start_server() {
         nohup java -jar ./target/kube-cloud-server-0.1.jar >kube-cloud-server-nohup.log 2>&1 &
         jps -l | grep "kube-cloud-server" | awk '{print $1}' > server.pid
+        echo "运行中的Java程序"
+        jps -l
 }
 
 clone_kube_cloud_server
